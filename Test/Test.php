f6d0d6a8a91504d30bb2e604b94dbb34499e8b6c
Mohamadka1 <124268603+Mohamadka1@users.noreply.github.com>
2023-10-02 11:28:05 +0200

    test3

--- Test/Test.php
+++ Test/Test.php
@@ -1,9 +1,18 @@
 
 
+
 <?php
-$test = 'test'
+    //calyie test
+    echo"hallo world";
+?>
+<?php
+$test = 'Nawaf test';
 ?>
 
 
 
 <strong style="color: red;"><?= $test?></strong>
+
+
+
+<h1>Mo test</h1>
